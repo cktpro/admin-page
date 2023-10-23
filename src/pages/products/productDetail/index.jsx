7f1057c7060bdb7232f4affc03eac8c725a16c32
cktpro <kimtramcap@gmail.com>
2023-10-23 23:11:17 +0700

    add product detail

--- src/pages/products/productDetail/index.jsx
+++ src/pages/products/productDetail/index.jsx
@@ -2,13 +2,14 @@ import React, { useCallback, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getProductDetail } from "api/productApi";
 import { Link } from "react-router-dom";
-import { Rate } from "antd";
+import { Rate, Progress } from "antd";
 import Loading from "components/loading";
 // Import style
 import "./productDetail.scss";
 const url = process.env.REACT_APP_BASE_URL_ADMIN;
 function ProductDetail(props) {
   const [isLoading, setIsLoading] = useState(null);
+  const [currentTab, setCurrentTab] = useState("btn-desc");
   const [product, setProduct] = useState([]);
   const params = useParams();
   const handleProductStock = useCallback((stock) => {
@@ -17,11 +18,17 @@ function ProductDetail(props) {
       return <p className="text-warning fw-bold">Sắp hết hàng</p>;
     return <p className="text-danger fw-bold">Hết hàng</p>;
   }, []);
-  const handleStatusProduct= useCallback((date)=>{
-    if(new Date(product.createdAt).getMonth() ===
-    new Date().getMonth()) return <p className="badge bg-info p-2">New</p>
-    return <p className="badge bg-secondary p-2">On Sale</p>
-},[product.createdAt])
+  const handleStatusProduct = useCallback(
+    (date) => {
+      if (new Date(product.createdAt).getMonth() === new Date().getMonth())
+        return <p className="badge bg-info p-2">New</p>;
+      return <p className="badge bg-secondary p-2">On Sale</p>;
+    },
+    [product.createdAt]
+  );
+  const selectTab = (event) => {
+    setCurrentTab(event.target.name);
+  };
   useEffect(() => {
     const getData = async () => {
       setIsLoading(true);
@@ -62,7 +69,7 @@ function ProductDetail(props) {
                 ? `${url}${product.image.location.split("public", 2)[1]}`
                 : require("assets/images/No-Image-Placeholder.png")
             }
-            alt={product?.image?`${product.image.name}`:"No image"}
+            alt={product?.image ? `${product.image.name}` : "No image"}
           />
 
           <div className="m-3">
@@ -80,23 +87,90 @@ function ProductDetail(props) {
                 <span>(330 review)</span>
               </div>
             </div>
-            <h2 >${product.price}</h2>
+            <h2>${product.price}</h2>
             <p>{product.description}</p>
           </div>
         </div>
         {/* Descripton section */}
-        <div className="border">
+        <div>
           <ul className="nav nav-tabs">
             <li className="nav-item">
-              <button className="nav-link active">Description</button>
+              <button
+                name="btn-desc"
+                onClick={selectTab}
+                className={` ${
+                  currentTab === "btn-desc" ? "nav-link active" : "nav-link"
+                }`}
+              >
+                Description
+              </button>
             </li>
             <li className="nav-item">
-              <button className="nav-link">Review</button>
+              <button
+                name="btn-review"
+                onClick={selectTab}
+                className={` ${
+                  currentTab === "btn-review" ? "nav-link active" : "nav-link"
+                }`}
+              >
+                Review
+              </button>
             </li>
             {/* <li className="nav-item">
     <a clasNames="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
   </li> */}
           </ul>
+
+          <div>
+            {currentTab === "btn-desc" ? (
+              <div className="p-2">
+                <h4>Specifications</h4>
+                <table className="table w-25">
+                  <tr>
+                    <td>Category:</td>
+                    <td>{product.category.name}</td>
+                  </tr>
+                  <tr>
+                    <td>Manufacture:</td>
+                    <td>{product.supplier.name}</td>
+                  </tr>
+                </table>
+                <h4>Delyvery and Return</h4>
+                <p>Your order of $200 or more gets free standard delivery</p>
+                <ul>
+                  <li>Standard delivered 4-5 Business Days</li>
+                  <li>Express delivered 2-4 Business Days</li>
+                </ul>
+                <p>
+                  Orders are processed and delivered Monday-Friday (excluding
+                  public holidays)
+                </p>
+              </div>
+            ) : (
+              <div>
+                <div className="rate-box">
+                  <div className="rate-score d-flex flex-column align-items-center justify-content-center">
+                    <p className="mb-1">Average rating</p>
+                    <h3 className="mb-1">4.5/5</h3>
+                    <Rate className="mb-1" allowHalf disabled value={4.5} />
+                    <p>(3k reviews)</p>
+                  </div>
+                  <div className="rate-rank d-flex align-items-center flex-column justify-content-center">
+                    <Progress percent={46} size="small" showInfo={false} />
+                    <Progress percent={29} size="small" showInfo={false} />
+                    <Progress percent={11} size="small" showInfo={false} />
+                    <Progress percent={10} size="small" showInfo={false} />
+                    <Progress percent={4} size="small" showInfo={false} />
+                  </div>
+                  <div className="rate-review d-flex align-items-center justify-content-center">
+                    <button type="button" className="btn btn-outline-secondary">
+                      Write you review
+                    </button>
+                  </div>
+                </div>
+              </div>
+            )}
+          </div>
         </div>
       </div>
     );
