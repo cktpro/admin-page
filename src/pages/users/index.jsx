725165cb373f18b26b38f79316875b260478bebe
thailehungvn <thailehungvn@gmail.com>
2023-11-01 21:43:50 +0700

    update

--- src/pages/users/index.jsx
+++ src/pages/users/index.jsx
@@ -1,116 +1,138 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { Table } from "antd";
 import { Link } from "react-router-dom";
 import { getUser } from "api/userApi";
 import Loading from "components/loading";
+import { axiosAdmin } from "helper/axiosAdmin/axiosAdmin";
 
 const url = process.env.REACT_APP_BASE_URL_ADMIN;
 
 
 const columns = [
+  // {
+  //   title: "Name",
+  //   render: (record) => {
+  //     if (record.image)
+  //       return (
+  //         <Link to={`/user_detail/${record._id}`}>
+  //           <div className="d-flex align-items-center img-user">
+  //             <img
+  //               src={`${url}${record.image.location.split("public", 2)[1]}`}
+  //               alt={record.image.name}
+  //               width="80px"
+  //               height="80px"
+  //               style={{ objectFit: "cover" }}
+  //             />
+  //             <div className="mx-2">
+  //               <p className="m-0 fw-bold">{record.name}</p>
+  //               <p className="m-0 text-black-50">{record.category.name}</p>
+  //             </div>
+  //           </div>
+  //         </Link>
+  //       );
+  //     //<img src={`http://localhost:3005${record.image.location.split('public',2)[1]}`} alt="" width="100px" height="100px"/>
+  //     return (
+  //       <Link to={`/user_detail/${record._id}`}>
+  //         <div className="d-flex align-items-center img-user">
+  //           <img  
+  //             src={require("assets/images/No-Image-Placeholder.png")}
+  //             alt=""
+  //             width="80px"
+  //             height="80px"
+  //             style={{ objectFit: "cover" }}
+  //           />
+  //           <div className="mx-2">
+  //             <p className="m-0 fw-bold">{record.name}</p>
+  //             <p className="m-0 text-black-50">{record.category.name}</p>
+  //           </div>
+  //         </div>
+  //       </Link>
+  //     );
+  //   },
+  //   filters: [
+  //     {
+  //       text: "Customers",
+  //       value: "Customers",
+  //     },
+  //     {
+  //       text: "Sellers",
+  //       value: "Sellers",
+  //     },
+  //     {
+  //       text: "Administrators",
+  //       value: "Administrators",
+  //     },
+  //     // {
+  //     //   text: "Registered Users)",
+  //     //   value: "RegisteredUsers)",
+  //     // },
+  //     // {
+  //     //   text: "Marketing and Advertising Group",
+  //     //   value: "MarketingandAdvertisingGroup",
+  //     // },
+  //     // {
+  //     //   text: "Customer support service)",
+  //     //   value: "Customersupportservice)",
+  //     // },
+
+
+  //     //   {
+  //     //     text: 'Category 1',
+  //     //     value: 'Category 1',
+  //     //     children: [
+  //     //       {
+  //     //         text: 'Yellow',
+  //     //         value: 'Yellow',
+  //     //       },
+  //     //       {
+  //     //         text: 'Pink',
+  //     //         value: 'Pink',
+  //     //       },
+  //     //     ],
+  //     //   },
+  //     //   {
+  //     //     text: 'Category 2',
+  //     //     value: 'Category 2',
+  //     //     children: [
+  //     //       {
+  //     //         text: 'Green',
+  //     //         value: 'Green',
+  //     //       },
+  //     //       {
+  //     //         text: 'Black',
+  //     //         value: 'Black',
+  //     //       },
+  //     //     ],
+  //     //   },  
+  //   ],
+  //   filterMode: "tree",
+  //   filterSearch: true,
+  //   onFilter: (value, record) => record.name.includes(value),
+  // },
+
   {
-    title: "Name",
-    render: (record) => {
-      if (record.image)
-        return (
-          <Link to={`/user_detail/${record._id}`}>
-            <div className="d-flex align-items-center img-user">
-              <img
-                src={`${url}${record.image.location.split("public", 2)[1]}`}
-                alt={record.image.name}
-                width="80px"
-                height="80px"
-                style={{ objectFit: "cover" }}
-              />
-              <div className="mx-2">
-                <p className="m-0 fw-bold">{record.name}</p>
-                <p className="m-0 text-black-50">{record.category.name}</p>
-              </div>
-            </div>
-          </Link>
-        );
-      //<img src={`http://localhost:3005${record.image.location.split('public',2)[1]}`} alt="" width="100px" height="100px"/>
-      return (
-        <Link to={`/user_detail/${record._id}`}>
-          <div className="d-flex align-items-center img-user">
-            <img  
-              src={require("assets/images/No-Image-Placeholder.png")}
-              alt=""
-              width="80px"
-              height="80px"
-              style={{ objectFit: "cover" }}
-            />
-            <div className="mx-2">
-              <p className="m-0 fw-bold">{record.name}</p>
-              <p className="m-0 text-black-50">{record.category.name}</p>
-            </div>
-          </div>
-        </Link>
-      );
-    },
+    title: "firstName",
+    dataIndex: "firstName", // Assuming "Company" is the property name in your data
     filters: [
       {
-        text: "Customers",
-        value: "Customers",
+        text: "Starts with 1",
+        value: "1",
       },
       {
-        text: "Sellers",
-        value: "Sellers",
+        text: "Starts with 2",
+        value: "2",
       },
-      {
-        text: "Administrators",
-        value: "Administrators",
-      },
-      // {
-      //   text: "Registered Users)",
-      //   value: "RegisteredUsers)",
-      // },
-      // {
-      //   text: "Marketing and Advertising Group",
-      //   value: "MarketingandAdvertisingGroup",
-      // },
-      // {
-      //   text: "Customer support service)",
-      //   value: "Customersupportservice)",
-      // },
-
-
-      //   {
-      //     text: 'Category 1',
-      //     value: 'Category 1',
-      //     children: [
-      //       {
-      //         text: 'Yellow',
-      //         value: 'Yellow',
-      //       },
-      //       {
-      //         text: 'Pink',
-      //         value: 'Pink',
-      //       },
-      //     ],
-      //   },
-      //   {
-      //     text: 'Category 2',
-      //     value: 'Category 2',
-      //     children: [
-      //       {
-      //         text: 'Green',
-      //         value: 'Green',
-      //       },
-      //       {
-      //         text: 'Black',
-      //         value: 'Black',
-      //       },
-      //     ],
-      //   },  
+      // Add more filter options as needed
     ],
-    filterMode: "tree",
+    onFilter: (value, record) => record.firstName.toString().startsWith(value),
     filterSearch: true,
-    onFilter: (value, record) => record.name.includes(value),
+    render: (id, record) => (
+      <Link to={`/details/${id}`}>{record.firstName}</Link>
+    ),
   },
   {
     title: "Phone Number",
-    dataIndex: "phonenumber", // Assuming phone number is the property name in your data
+    dataIndex: "phoneNumber", // Assuming phone number is the property name in your data
     filters: [
       {
         text: "Starts with 1",
@@ -122,32 +144,16 @@ const columns = [
       },
       // Add more filter options as needed
     ],
-    onFilter: (value, record) => record.phonenumber.toString().startsWith(value),
+    onFilter: (value, record) => record["Phone Number"].toString().startsWith(value),
     filterSearch: true,
     render: (id, record) => (
-      <Link to={`/details/${id}`}>{(record.phonenumber * (100 - record.phonenumber)) / 100}</Link>
+      <Link to={`/details/${id}`}>{record["Phone Number"]}</Link>
     ),
   },
-  {
-    title: "Company",
-    dataIndex: "Company", // Assuming "Company" is the property name in your data
-    filters: [
-      {
-        text: "Starts with 1",
-        value: "1",
-      },
-      {
-        text: "Starts with 2",
-        value: "2",
-      },
-      // Add more filter options as needed
-    ],
-    onFilter: (value, record) => record.Company.toString().startsWith(value),
-    filterSearch: true,
-    render: (id, record) => (
-      <Link to={`/details/${id}`}>{(record.Company * (100 - record.Company)) / 100}</Link>
-    ),
-  },
+
+
+ 
+  
   {
     title: "Role",
     dataIndex: "Role", // Assuming "Role" is the property name in your data
@@ -163,27 +169,28 @@ const columns = [
       // Add more filter options as needed
     ],
     onFilter: (value, record) => record.Role.toString().startsWith(value),
-    filterSearch: true,
-    render: (id, record) => (
-      <Link to={`/details/${id}`}>{(record.Role * (100 - record.Role)) / 100}</Link>
-    ),
+  filterSearch: true,
+  render: (id, record) => (
+    <Link to={`/details/${id}`}>{record.Role}</Link>
+  ),
   },
+
   {
     title: "Trạng thái",
     dataIndex: "status",
-    render: (status) => (
+    render: (record) => (
       <span
         className={`${
-          status === "banned"
+          record === "banned"
             ? "badge bg-danger"
-            : status === "pending"
+            : record === "pending"
             ? "badge bg-warning"
-            : status === "active"
+            : record === "active"
             ? "badge bg-success"
             : ""
         }`}
       >
-        {status}
+        {record}
       </span>
     ),
     sorter: (a, b) => a.status.localeCompare(b.status),
@@ -216,6 +223,17 @@ const columns = [
 ];
 
 function UserList() {
+  const [userList,setUserList]= useState([])
+  console.log('««««« userList »»»»»', userList);
+  const getUser = useCallback( async()=>{
+    try {
+      const res = await axiosAdmin.get('/customers')
+      console.log('««««« res »»»»»', res);
+      setUserList(res.data.payload)
+    } catch (error) {
+      console.log('««««« error »»»»»', error);
+    }
+  },[])
   const [isLoading, setIsLoading] = useState(null);
   const [user, setUser] = useState([]);
   const [selectedRowKeys, setSelectedRowKeys] = useState([]);
@@ -229,13 +247,16 @@ function UserList() {
   };
 
   useEffect(() => {
-    const fetchData = async () => {
-      setIsLoading(true);
-      const res = await getUser();
-      setUser(res.data.payload);
-      setIsLoading(false);
-    };
-    fetchData();
+    getUser()
+   
+    // const fetchData = async () => {
+      // setIsLoading(true);
+    //   const res = await getUser();
+    //   setUser(res.data.payload);
+      // setIsLoading(false);
+    // };
+    // fetchData();
+
   }, []);
   return (
     <>
@@ -257,7 +278,7 @@ function UserList() {
           rowKey="_id"
           rowSelection={rowSelection}
           columns={columns}
-          dataSource={user}
+          dataSource={userList}
         />
       ) : (
         <Loading />
