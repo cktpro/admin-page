bc3d6744e8a77c4cf7779a52b427e5819d8411df
cktpro <kimtramcap@gmail.com>
2023-11-16 14:41:46 +0700

    add function add product

--- src/pages/products/createProduct/index.jsx
+++ src/pages/products/createProduct/index.jsx
@@ -54,23 +54,81 @@ function CreateProduct(props) {
     };
     getData();
   }, []);
-   const asyncForEach= async (array, callback) => {
+  const asyncForEach = async (array, callback) => {
     for (let index = 0; index < array.length; index += 1) {
       await callback(array[index], index, array); // eslint-disable-line
     }
   };
   const onFinish = useCallback(async (values) => {
-    const formData = new FormData();
-    formData.append("file", values.upload.file);
-    console.log('◀◀◀ values ▶▶▶',values.images.fileList);
-    let list=[]
-    asyncForEach(values.images.fileList,(arrayindex,index,array)=>{
-      console.log('◀◀◀ a ▶▶▶');
-    })
+    try {
+      const formData = new FormData();
+      formData.append("file", values.upload.file);
+      const img = await axiosAdmin.post("/media/upload-single", formData);
+      let list = [];
+      await asyncForEach(
+        values.images.fileList,
+        async (arrayindex, index, array) => {
+          // list.push(arrayindex.uid)
+          // console.log('◀◀◀ list ▶▶▶',list);
+          formData.append("file", arrayindex.originFileObj);
+          axiosAdmin.defaults.headers.common["Authorization"] =
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3MDAxMTYxNDYsIl9pZCI6IjY1NDlkOTNmOWVmNTI1ZGU1MzU5MzE0NSIsImZpcnN0TmFtZSI6IkPDoXAiLCJsYXN0TmFtZSI6IktpbSBUcuG6p20iLCJwaG9uZU51bWJlciI6Ijg0MDM1NzA4MTE4NiIsImFkZHJlc3MiOiJRdeG6o25nIFRy4buLIiwiZW1haWwiOiJja3Rwcm9AZ21haWwuY29tIiwiYmlydGhkYXkiOiIxOTk5LTAzLTI0VDE3OjAwOjAwLjAwMFoiLCJ1cGRhdGVkQXQiOiIyMDIzLTExLTA3VDA2OjI5OjE5Ljc5M1oiLCJhbGdvcml0aG0iOiJIUzI1NiIsImV4cCI6MTcwMDIwMjU0Nn0.KJm6Qkf828ZT87fBR5sGXY4C330his1bf7wruEJEyps";
+          const res = await axiosAdmin.post("/media/upload-single", formData);
+          console.log("◀◀◀ res ▶▶▶", res);
+          list.push({
+            mediaId: res.data.payload.id,
+            location: res.data.payload.location,
+          });
+          console.log("◀◀◀ list ▶▶▶", list);
+        }
+      );
+      const productData = {
+        name: values.name,
+        price: values.price,
+        discount: values.discount,
+        stock: values.stock,
+        width: values.width,
+        height: values.height,
+        weight: values.weight,
+        length: values.length,
+        description: values.description,
+        categoryId: values.categoryId,
+        supplierId: values.supplierId,
+        coverImg: `${img.data.payload._id}`,
+        imageList: list,
+        // mediaId: "6550baa8446ae47a099208f0",
+      };
+      await axiosAdmin
+        .post("/products", productData)
+        .then((res) => {
+          message.success("Thêm sản phẩm thành công")
+          console.log("◀◀◀ res ▶▶▶", res);
+        })
+        .catch(async (err) => {
+          message.error("Thêm sản phẩm thất bại")
+          console.log('◀◀◀ Upload produts err ▶▶▶',err);
+          await axiosAdmin.delete(`/media/${img.data.payload._id}`).then((res)=>{
+            console.log('◀◀◀ Xóa cover image thành công ▶▶▶');
+          }).catch((err)=>{
+            console.log('◀◀◀ Xóa cover image thất bại ▶▶▶',err);
+          })
+          await asyncForEach(list, async (arrayindex, index, array) => {
+            // list.push(arrayindex.uid)
+            // console.log('◀◀◀ list ▶▶▶',list);
+            axiosAdmin.defaults.headers.common["Authorization"] =
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3MDAxMTYxNDYsIl9pZCI6IjY1NDlkOTNmOWVmNTI1ZGU1MzU5MzE0NSIsImZpcnN0TmFtZSI6IkPDoXAiLCJsYXN0TmFtZSI6IktpbSBUcuG6p20iLCJwaG9uZU51bWJlciI6Ijg0MDM1NzA4MTE4NiIsImFkZHJlc3MiOiJRdeG6o25nIFRy4buLIiwiZW1haWwiOiJja3Rwcm9AZ21haWwuY29tIiwiYmlydGhkYXkiOiIxOTk5LTAzLTI0VDE3OjAwOjAwLjAwMFoiLCJ1cGRhdGVkQXQiOiIyMDIzLTExLTA3VDA2OjI5OjE5Ljc5M1oiLCJhbGdvcml0aG0iOiJIUzI1NiIsImV4cCI6MTcwMDIwMjU0Nn0.KJm6Qkf828ZT87fBR5sGXY4C330his1bf7wruEJEyps";
+            const res = await axiosAdmin.delete(`/media/${arrayindex.mediaId}`);
+            console.log("◀◀◀ res ▶▶▶", res);
+          });
+        });
+    } catch (error) {
+      message.error("Lỗi khi thêm sản phẩm")
+      console.log("◀◀◀ error ▶▶▶", error);
+    }
 
     // try {
     //   axiosAdmin.defaults.headers.common["Authorization"] =
-    //     "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2OTk3ODcyOTYsIl9pZCI6IjY1NGIwYzgxMDliMDViOTE2ZTQwMTIyNCIsImZpcnN0TmFtZSI6Ik5ndXnhu4VuIiwibGFzdE5hbWUiOiJBbiIsImVtYWlsIjoiYW5uZ3V5ZW5AZ21haWwuY29tIiwiYmlydGhkYXkiOiIxOTk5LTAzLTI0VDE3OjAwOjAwLjAwMFoiLCJ1cGRhdGVkQXQiOiIyMDIzLTExLTA4VDA0OjIwOjE3LjU1MloiLCJyb2xlIjoiVVNFUiIsImFsZ29yaXRobSI6IkhTMjU2IiwiZXhwIjoxNjk5ODczNjk2fQ.XfuYKf9fbfXCFeiMp64JGXqTIXAYVcBKdhpGZs9rJk4";
+    //     "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3MDAxMTYxNDYsIl9pZCI6IjY1NDlkOTNmOWVmNTI1ZGU1MzU5MzE0NSIsImZpcnN0TmFtZSI6IkPDoXAiLCJsYXN0TmFtZSI6IktpbSBUcuG6p20iLCJwaG9uZU51bWJlciI6Ijg0MDM1NzA4MTE4NiIsImFkZHJlc3MiOiJRdeG6o25nIFRy4buLIiwiZW1haWwiOiJja3Rwcm9AZ21haWwuY29tIiwiYmlydGhkYXkiOiIxOTk5LTAzLTI0VDE3OjAwOjAwLjAwMFoiLCJ1cGRhdGVkQXQiOiIyMDIzLTExLTA3VDA2OjI5OjE5Ljc5M1oiLCJhbGdvcml0aG0iOiJIUzI1NiIsImV4cCI6MTcwMDIwMjU0Nn0.KJm6Qkf828ZT87fBR5sGXY4C330his1bf7wruEJEyps";
     //   await axiosAdmin
     //     .post("/media/upload-single", formData)
     //     .then(async (img) => {
@@ -229,10 +287,14 @@ function CreateProduct(props) {
                 name="stock"
                 rules={[
                   { required: true, message: "Name is required" },
-                  { type:"number" ,min: 1, message: "Please enter a stock from 1" },
+                  {
+                    type: "number",
+                    min: 1,
+                    message: "Please enter a stock from 1",
+                  },
                 ]}
               >
-                <InputNumber style={{ width: "100%" }}  />
+                <InputNumber style={{ width: "100%" }} />
               </Form.Item>
             </div>
             <div className=" col-xs-12 col-lg-6 col-sm-6">
@@ -259,7 +321,12 @@ function CreateProduct(props) {
             name="upload"
             rules={[{ required: true, message: "Missing cover image" }]}
           >
-            <Upload name="file" maxCount={1} beforeUpload={true} listType="picture">
+            <Upload
+              name="file"
+              maxCount={1}
+              beforeUpload={true}
+              listType="picture"
+            >
               <Button icon={<UploadOutlined />}>Cover image</Button>
             </Upload>
           </Form.Item>
@@ -269,7 +336,7 @@ function CreateProduct(props) {
             name="images"
             rules={[{ required: true, message: "Missing cover image" }]}
           >
-            <Upload name="list" beforeUpload={true}  multiple listType="picture">
+            <Upload name="list" beforeUpload={true} multiple listType="picture">
               <Button icon={<UploadOutlined />}>Image list</Button>
             </Upload>
           </Form.Item>
@@ -281,14 +348,13 @@ function CreateProduct(props) {
           <div className="d-flex flex-column gap-2">
             <div className="d-flex align-items-center justify-content-start">
               <span>Product Spec</span>
-    
             </div>
 
             <div className="row">
               <div className="col-xs-12 col-lg-6 col-sm-6">
                 <Form.Item
-                label="Width"
-                  name= "width"
+                  label="Width"
+                  name="width"
                   rules={[
                     {
                       type: "number",
@@ -298,16 +364,19 @@ function CreateProduct(props) {
                     { required: true, message: "Width is required" },
                   ]}
                 >
-                  <InputNumber   placeholder="Width(cm)" style={{width:"100%"}} />
+                  <InputNumber
+                    placeholder="Width(cm)"
+                    style={{ width: "100%" }}
+                  />
                 </Form.Item>
                 <Form.Item
                   label="Height"
-                  name= "height"
+                  name="height"
                   rules={[
                     {
-                      type:"number",
-                      min:0,
-                      message:"Height is invalid"
+                      type: "number",
+                      min: 0,
+                      message: "Height is invalid",
                     },
                     {
                       required: true,
@@ -315,18 +384,21 @@ function CreateProduct(props) {
                     },
                   ]}
                 >
-                  <InputNumber   placeholder="Height(cm)" style={{width:"100%"}} />
+                  <InputNumber
+                    placeholder="Height(cm)"
+                    style={{ width: "100%" }}
+                  />
                 </Form.Item>
               </div>
               <div className="col-xs-12 col-lg-6 col-sm-6">
                 <Form.Item
                   label="Weight"
-                  name= "weight"
+                  name="weight"
                   rules={[
                     {
-                      type:"number",
-                      min:0,
-                      message:"Weight is invalid"
+                      type: "number",
+                      min: 0,
+                      message: "Weight is invalid",
                     },
                     {
                       required: true,
@@ -334,16 +406,19 @@ function CreateProduct(props) {
                     },
                   ]}
                 >
-                  <InputNumber placeholder="Weight(gram)" style={{width:"100%"}} />
+                  <InputNumber
+                    placeholder="Weight(gram)"
+                    style={{ width: "100%" }}
+                  />
                 </Form.Item>
                 <Form.Item
                   label="Length"
-                  name= "length"
+                  name="length"
                   rules={[
                     {
-                      type:"number",
-                      min:0,
-                      message:"Length is invalid"
+                      type: "number",
+                      min: 0,
+                      message: "Length is invalid",
                     },
                     {
                       required: true,
@@ -351,11 +426,13 @@ function CreateProduct(props) {
                     },
                   ]}
                 >
-                  <InputNumber placeholder="Length(cm)" style={{width:"100%"}} />
+                  <InputNumber
+                    placeholder="Length(cm)"
+                    style={{ width: "100%" }}
+                  />
                 </Form.Item>
               </div>
-              <div className="row">
-              </div>
+              <div className="row"></div>
             </div>
           </div>
           {/* <Form.Item>
