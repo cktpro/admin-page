857a13ffda57ac649536cb7a426bf636508cebe5
Man Nguyen <128272683+ChuPhongMCP@users.noreply.github.com>
2023-12-05 16:10:56 +0700

    update reload page

--- src/app/App.js
+++ src/app/App.js
@@ -14,6 +14,16 @@ function App() {
     }
   }, [navigate, token]);
 
+  window.onbeforeunload = (event) => {
+    const e = event || window.event;
+    // Cancel the event
+    e.preventDefault();
+    if (e) {
+      e.returnValue = ''; // Legacy method for cross browser support
+    }
+    return ''; // Legacy method for cross browser support
+  };
+
   const renderRoutes = useCallback((routers) => {
     if (!token) { //router khi chưa đăng nhập
       return unAuthRouter.map((route, index) => {
